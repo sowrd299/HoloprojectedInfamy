1530e272c070d73166c9327ccb7b668994df7e26
sowrd299 <ryan.ward46@gmail.com>
2020-12-24 08:35:43 -0800

    Adds a game app

--- ElevenSystemsSite/settings.py
+++ ElevenSystemsSite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ElevenSystemsSite/urls.py
+++ ElevenSystemsSite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
+    path('game/', include('game.urls', namespace='game')),
 ]
