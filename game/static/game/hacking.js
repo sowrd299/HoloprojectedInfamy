253e44c5eeeae5b440d4e3663a76114e282db9f3
sowrd299 <ryan.ward46@gmail.com>
2020-12-28 17:38:28 -0800

    Adds JS for interact with the game state and moving around the map

---
+++ game/static/game/hacking.js
@@ -0,0 +1,150 @@
+let nodes;
+let node_elements;
+
+let tools;
+let tool_elements;
+
+let link_elements;
+
+let playerNode; // the node where they player's terminal is
+let targetNode; // the node the player is targeting
+let enemyNodes; // a set of all nodes with enemies
+
+let area;
+
+
+/*
+Traverses the screen and targeting system across a link
+*/
+function traverseLink(link_element) {
+    nodeIds = link_element.dataset.nodes.split(" ");
+    for(let i = 0; i < nodeIds.length; i++){
+        let nodeId = nodeIds[i];
+        let node = nodes[nodeId];
+        if(node != targetNode){
+            targetNode = node;
+            break;
+        }
+    };
+    display();
+}
+
+
+/*
+Sets the visibility of the given element to the given value
+*/
+function setVisible(element, visible) {
+    if(visible){
+        element.classList.remove("hidden");
+    }else{
+        element.classList.add("hidden");
+    }
+}
+
+
+/*
+To be run when the page loads
+*/
+function setup() {
+
+    area = document.getElementById("hackingarea");
+
+    // nodes data structure
+    nodes = {};
+    enemyNodes = new Set();
+
+    nodes_json.forEach(node => {
+        nodes[node.pk] = node;
+        node.linked_nodes = new Set();
+
+        node.is_visible = !node.fields.is_secret;
+
+        if(node.fields.is_player_start){
+            playerNode = node;
+        }
+
+        if(node.fields.is_enemy_node){
+            enemyNodes.add(node);
+        }
+
+    });
+
+    targetNode = playerNode;
+
+
+    // tools data scructrue
+    tools = {};
+
+    tools_json.forEach(tool => {
+        tool[tool.pk] = tool;
+    });
+
+
+    // links data structure
+    links_json.forEach(link => {
+        node_to = nodes[link.fields.node_to];
+        node_from = nodes[link.fields.node_from];
+
+        node_to.linked_nodes.add(node_from);
+        node_from.linked_nodes.add(node_to);
+    });
+
+
+    // initialize the display
+
+    node_elements = document.getElementsByClassName("hackingnode");
+    for(let i = 0; i < node_elements.length; i++){
+        let terminals = node_elements.item(i).getElementsByClassName("hackingterminal");
+        for(let j = 0; j < terminals.length; j++){
+            setVisible(terminals.item(j), false)
+        }
+
+        setVisible(node_elements.item(i), nodes[node_elements.item(i).id].is_visible);
+    }
+
+    tool_elements = document.getElementsByClassName("hackingtool");
+
+    link_elements = document.getElementsByClassName("hackinglink");
+    for(let i = 0; i < link_elements.length; i++) {
+        let link_element = link_elements.item(i);
+        // links as bttons
+        link_element.onclick = function(event){
+            traverseLink(link_element);
+        }
+        // hide links to hidden places
+        let linked_nodes = link_element.dataset.nodes.split(" ") ;
+        for(let j = 0; j < linked_nodes.length; j++){
+            if(!nodes[linked_nodes[j]].is_visible){
+                setVisible(link_element, false);
+                break;
+            }
+        }
+    }
+
+    // run the display
+    display();
+
+}
+
+
+/*
+To be run whenever the gamestate changes
+*/
+function display() {
+
+    // update center the target node
+    area.style.left = (area.clientWidth/2 - targetNode.fields.x_pos)+"px";
+    area.style.top = (area.clientHeight/2 - targetNode.fields.y_pos)+"px";
+
+    // update the terminal displays
+    for(let i = 0; i < node_elements.length; i++)  {
+        let node_element = node_elements.item(i);
+        let node = nodes[node_element.id];
+
+        setVisible(node_element.querySelector("#player"), node == playerNode);
+        setVisible(node_element.querySelector("#enemy"), enemyNodes.has(node));
+    }
+
+}
+
+window.onload = setup;
--- game/views.py
+++ game/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
 from .models import *
+
 
 # Create your views here.
 
 def index(request):
     hacking_map = 'Test'
+
+    # load the gamestate into the context
     context = {
         'nodes' : HackingNode.objects.filter(map_in__short_name = hacking_map),
         'links' : HackingLink.objects.filter(node_from__map_in__short_name = hacking_map),
         'tools' : HackingTool.objects.all(),
     }
+
+    # creates json representations for the game state
+    json_context = { '{}_json'.format(k) : serialize('json', v) for k, v in context.items() }
+    context.update(json_context)
+
     return render(request, 'game/index.html', context)
